d54f54557a30ba6f2f62e46cfcbda893c9ab5172
tapasbaditya <tapasbaditya@gmail.com>
2023-09-11 14:57:54 +0530

    ng upgrad to 16

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,5 +11,29 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  onClkChange() {
+    var ele = document.getElementById('change') as HTMLElement
+    ele.classList.add('menu-highlight')
+  }
+  onClkChange2() {
+    var ele = document.getElementById('change') as HTMLElement
+    ele.classList.remove('menu-highlight')
+
+  }
+
+  addAll() {
+    var x = document.getElementsByClassName('menu') as HTMLCollection
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.add('menu-highlight');
+    }
+
+  }
+
+  removeAll() {
+    var x = document.getElementsByClassName('menu') as HTMLCollection
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove('menu-highlight');
+    }
+  }
 
 }
--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -10,34 +10,62 @@ export class NavBarComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-  onClkHome() {
-    var ele = document.getElementById('home') as HTMLElement
-    ele.classList.add('menu-highlight')
-  }
-
-  onClkProducts() {
-    var ele = document.getElementById('product') as HTMLElement
-    ele.classList.add('menu-highlight')
 
   }
 
-  onClkOrders() {
-    var ele = document.getElementById('orders') as HTMLElement
+  // onClkHome() {
+  //   var x = document.getElementsByClassName('menu') as HTMLCollection
+  //   for (var i = 0; i < x.length; i++) {
+  //     x[i].classList.remove('menu-highlight');
+  //   }
+  //   var ele1 = document.getElementById('home') as HTMLElement
+  //   ele1.classList.add('menu-highlight')
+  // }
+
+  // onClkOrders() {
+  //   var x = document.getElementsByClassName('menu') as HTMLCollection
+  //   for (var i = 0; i < x.length; i++) {
+  //     x[i].classList.remove('menu-highlight');
+  //   }
+  //   var ele2 = document.getElementById('orders') as HTMLElement
+  //   ele2.classList.add('menu-highlight')
+  // }
+
+  // onClkProducts() {
+  //   var x = document.getElementsByClassName('menu') as HTMLCollection
+  //   for (var i = 0; i < x.length; i++) {
+  //     x[i].classList.remove('menu-highlight');
+  //   }
+  //   var ele3 = document.getElementById('product') as HTMLElement
+  //   ele3.classList.add('menu-highlight')
+  // }
+
+
+  // onClkManage() {
+  //   var x = document.getElementsByClassName('menu') as HTMLCollection
+  //   for (var i = 0; i < x.length; i++) {
+  //     x[i].classList.remove('menu-highlight');
+  //   }
+  //   var ele4 = document.getElementById('manage') as HTMLElement
+  //   ele4.classList.add('menu-highlight')
+  // }
+
+  // onClkAccount() {
+  //   var x = document.getElementsByClassName('menu') as HTMLCollection
+  //   for (var i = 0; i < x.length; i++) {
+  //     x[i].classList.remove('menu-highlight');
+  //   }
+  //   var ele5 = document.getElementById('account') as HTMLElement
+  //   ele5.classList.add('menu-highlight')
+  // }
+
+  onClkMenubar(menuid: string){
+    var x = document.getElementsByClassName('menu') as HTMLCollection
+    for(var i = 0; i < x.length; i++){
+      x[i].classList.remove('menu-highlight');
+    }
+    var ele = document.getElementById(menuid) as HTMLElement
     ele.classList.add('menu-highlight')
-  }
-
-  onClkManage() {
-    var ele = document.getElementById('manage') as HTMLElement
-    ele.classList.add('menu-highlight')
-
-  }
-
-  onClkAccount() {
-    var ele = document.getElementById('account') as HTMLElement
-    ele.classList.add('menu-highlight')
-
   }
 
 }
